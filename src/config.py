aca506546f70e786be31cf05b3113e9022cf11b2
It Soda <nikshchegilskiy@gmail.com>
2023-12-10 00:23:49 +0300

    feat| add docker | update order router

--- src/config.py
+++ src/config.py
@@ -9,3 +9,8 @@ DB_USER = os.environ.get("DB_USER")
 DB_PASSWORD = os.environ.get("DB_PASSWORD")
 DB_HOST = os.environ.get("DB_HOST")
 DB_PORT = os.environ.get("DB_PORT")
+
+if os.environ.get("DOCKER_IS_RUN"):
+    DB_HOST = "mysql_fastapi"
+    DB_USER = "itsoda"
+    DB_PASSWORD = "test_pass"
